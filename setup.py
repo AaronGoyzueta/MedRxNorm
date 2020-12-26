6d8126acab3db24d9d2bff61854fbbf7a19609b9
Aaron Goyzueta <aarongoyzueta@Aarons-MacBook-Air.local>
2020-12-26 12:08:14 -0500

    Updated dependancies and project links

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+project_urls = {
+  'GitHub Link': 'https://github.com/AaronGoyzueta/MedRxNorm'
+}
+
 setuptools.setup(
     name="MedRxNorm",
-    version="0.0.1",
+    version="0.0.3",
     author="Aaron Goyzueta",
     author_email="aarongoyzueta@gmail.com",
     description="Python module for normalizing medical prescriptions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    install_requires=["pynini", "num2words", "textblob", "text2digits"],
+    project_ruls=project_urls
 )
